ddc57109ba26c0a77bfbd382113bcf3c392bdaaf
amandabeers <amanda.green413@gmail.com>
2019-12-03 11:44:35 -0500

    Create file for CreatePost component

---
+++ src/components/Posts/CreatePost.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import { withRouter } from 'react-router-dom'
+// import axios from 'axios'
+// import apiUrl from './../../apiConfig'
+
+class CreatePost extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      post: {
+        file: '',
+        description: '',
+        name: ''
+      }
+    }
+  }
+}

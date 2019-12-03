41541f02f5e05114805aaca3f1041545a6c29a4c
amandabeers <amanda.green413@gmail.com>
2019-12-03 16:44:24 -0500

    Add create post feature
    -made form to create a post with a file upload for a photo
    -added an authenticated route for creating a post
    -added a link to the form on the authenticated nav bar
    -confirmed that images are being uploaded to aws and a post is being
    created on the backend with the url saved

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
+import CreatePost from '../Posts/CreatePost'
 
 class App extends Component {
   constructor () {
@@ -53,6 +54,9 @@ class App extends Component {
           )} />
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword alert={this.alert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} path='/create-post' render={() => (
+            <CreatePost alert={this.alert} user={user} />
           )} />
         </main>
       </Fragment>
--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 // import { withRouter } from 'react-router-dom'
-// import axios from 'axios'
-// import apiUrl from './../../apiConfig'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+import apiUrl from './../../apiConfig'
 
 class CreatePost extends Component {
   constructor () {
@@ -10,9 +12,86 @@ class CreatePost extends Component {
     this.state = {
       post: {
         file: '',
-        description: '',
-        name: ''
+        description: ''
       }
     }
   }
+
+  handleChange = (event) => {
+    this.setState({ post: { ...this.state.post, [event.target.name]: [event.target.value] } })
+    // console.log(this.state)
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    // for (const [key, value] of formData.entries()) {
+    //   console.log(key, value)
+    // }
+    try {
+      const res = await axios({
+        url: `${apiUrl}/posts`,
+        method: 'POST',
+        headers: {
+          'Authorization': `Token token=${this.props.user.token}`
+        },
+        data: formData
+      })
+      this.props.alert({ heading: 'Success!',
+        message: 'Your character has been created',
+        variant: 'success'
+      })
+      console.log(res)
+      // this.props.history.push(`/posts/${res.data.post._id}`)
+    } catch (error) {
+      this.props.alert({ heading: 'Error',
+        message: 'Oops! Something went wrong.',
+        variant: 'danger'
+      })
+    }
+  }
+
+  render () {
+    const { post } = this.state
+    return (
+      <div>
+        <h3 className="text-center mt-2 mb-2">Create a New Post</h3>
+        <div className="form-wrapper">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="file" encType="multipart/form-data">
+              <Form.Label>Photo Upload</Form.Label>
+              <Form.Control
+                name="file"
+                type="file"
+                placeholder="Upload Photo"
+                value={post.file}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="description">
+              <Form.Label>Photo Caption</Form.Label>
+              <Form.Control
+                name="description"
+                type="text"
+                placeholder="Caption"
+                value={post.description}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mr-2">
+              Submit
+            </Button>
+            <Button variant="danger" type="button" as="a" href='#'>
+              Cancel
+            </Button>
+          </Form>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default CreatePost
